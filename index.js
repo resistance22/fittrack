d984705840d1432e0d1120357c02a235b2d6aa58
Amin Foroutan <aminforoutan7@gmail.com>
2021-03-07 18:12:59 +0330

    set the view engine to ejs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', paht.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
