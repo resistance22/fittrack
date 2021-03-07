e1a872a4f6b959a87934c5bd286e19f3f59072cc
Amin Foroutan <aminforoutan7@gmail.com>
2021-03-07 18:58:11 +0330

    modulirized the routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -9,10 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Fittrack',
-  });
-});
+// setting up the routes
+app.use(routes());
 
 app.listen(port, () => console.log(`Fittrack app listening at http://localhost:${port}`));
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+
+module.exports = () => {
+  router.get('/', (req, res) => {
+    res.render('home', {
+      title: 'Fittrack',
+    });
+  });
+  return router;
+};
