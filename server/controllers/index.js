494de33200350c072a016acc6cf40036d27d64aa
amin foroutan <aminforoutan7@gmail.com>
2021-03-08 22:24:15 +0330

    added a seperate controller for api

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,15 @@
+const registerControllers = require('./api/registerController');
 module.exports = {
   home: {
     get: (req, res) => {
       res.render('home', {
         title: 'Fitrack',
       });
+    },
+    api: {
+      register: {
+        post: registerControllers.post,
+      },
     },
   },
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const routes = require('./routes');
+const api = require('./api');
 
 const app = express();
 const port = process.env.PORT || 2000;
@@ -17,5 +18,9 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 // setting up the routes
 app.use(routes());
+
+
+// setup api endpoints
+app.use('/api', api());
 
 app.listen(port, () => console.log(`Fittrack app listening at http://localhost:${port}`));
