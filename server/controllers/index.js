4f2cd09b4d88ae4e7bf60692721e0c9c1f07709a
amin foroutan <aminforoutan7@gmail.com>
2021-03-09 20:39:59 +0330

    [bug]moved the api controller out of home

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,21 @@
 const registerControllers = require('./api/registerController');
+const testAPIController = require('./api/testController');
 module.exports = {
+  // Home Route Controllers
   home: {
     get: (req, res) => {
       res.render('home', {
         title: 'Fitrack',
       });
     },
-    api: {
-      register: {
-        post: registerControllers.post,
-      },
+  },
+  // All api controllers
+  api: {
+    test: {
+      get: testAPIController.get,
+    },
+    register: {
+      post: registerControllers.post,
     },
   },
 };
