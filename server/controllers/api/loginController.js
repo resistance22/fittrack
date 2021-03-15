6d2d5248dfe2aa8da6bf8a564e3e8f5389320c32
Amin Foroutan <aminforoutan7@gmail.com>
2021-03-15 22:12:44 +0330

    added crudentials checking to loginContorller

--- server/controllers/api/loginController.js
+++ server/controllers/api/loginController.js
@@ -4,7 +4,7 @@ const UserModel = require('../../models/userModel');
 module.exports = {
   /**
    * register api controller
-   * @api /api/register
+   * @api /api/login
    * @param  {Object} req
    * @param  {Object} res
    */
@@ -18,6 +18,28 @@ module.exports = {
     if ( !( (email || phonenumber) && password ) ) {
       return res.status(422).json({
         error: 'missing value',
+      });
+    }
+    const user = await UserModel.findOneByEmailOrPhonenumber(
+        email,
+        phonenumber,
+    );
+    if ( user ) {
+      const isPasswordCorrect = await user.isUserPasswordCorrect(password);
+      if ( isPasswordCorrect) {
+        return res.status(200).json({
+          id: user.ID,
+          email: user.email,
+          phonenumber: user.phonenumber,
+        });
+      } else {
+        return res.status(401).json({
+          error: 'wrong password',
+        });
+      }
+    } else {
+      return res.status(401).json({
+        error: 'wrong email or phone number',
       });
     }
   },
